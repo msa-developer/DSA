14843e87466e50750bbb51d47e86363f08146c9b
MSA611 <shoaib9542186823@gmail.com>
2025-10-13 21:39:03 +0530

    added file of selection sort

---
+++ selectionSort.java
@@ -0,0 +1,39 @@
+// Example : 13 46 24 52 20 9
+// The output should be :
+// 9
+// 13
+// 20
+// 24
+// 46
+// 52
+// In Selection Sort We Select Minimum Element Of The Array And Then Sort
+
+public class selectionSort {
+
+  public static void swap(int arr[], int a, int b) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+
+  public static void sort(int arr[]) {
+    for (int i = 0; i < arr.length - 1; i++) {
+      int index = i;
+      for (int j = i; j < arr.length; j++) {
+        if (arr[j] < arr[index])
+          index = j;
+      }
+      int temp = arr[index];
+      arr[index] = arr[i];
+      arr[i] = temp;
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) {
+    sort(new int[] {13, 46, 24, 52, 20, 9});
+  }
+}
