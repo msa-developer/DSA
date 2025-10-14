5a8bc8ae2e3642c458794cec909a1dd2d7c40281
MSA611 <shoaib9542186823@gmail.com>
2025-10-14 07:33:53 +0530

    removed unnecessary swap function

--- selectionSort.java
+++ selectionSort.java
@@ -11,12 +11,6 @@
 
 public class selectionSort {
 
-  public static void swap(int arr[], int a, int b) {
-    int temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
-  }
-
   public static void sort(int arr[]) {
     for (int i = 0; i < arr.length - 1; i++) {
       int index = i;
