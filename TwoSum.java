926e965771a7fa76bd27aa93f76e5547fea3712d
msa-developer <shoaib9542186823@gmail.com>
2026-02-10 17:54:53 +0530

    167. Two Sum II - Input Array Is Sorted

---
+++ TwoSum.java
@@ -0,0 +1,27 @@
+
+/**
+ * TwoSum
+ */
+public class TwoSum {
+
+  public static int[] pointerAppTwo(int[] arr, int target) {
+    int i = 0;
+    int j = arr.length - 1;
+    int sum = 0;
+    while (i < j) {
+      sum = arr[i] + arr[j];
+      if (sum > target) {
+        j--;
+      } else if (sum < target) {
+        i++;
+      } else if (sum == target) {
+        return new int[] { (i + 1), (j + 1) };
+      }
+    }
+    return new int[] { -1, -1 };
+  }
+
+  public static void main(String[] args) {
+    pointerAppTwo(new int[] { 2, 3, 4 }, 6);
+  }
+}
