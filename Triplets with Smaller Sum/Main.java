5b0dcb54c1166f089b8f8a0b8fceaf7f9d50c7c3
msa-developer <shoaib9542186823@gmail.com>
2026-02-13 11:22:36 +0530

    started another working for solution

---
+++ Triplets with Smaller Sum/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main
+ */
+public class Main {
+
+  public static long solution(int n, int sum, long arr[]) {
+
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    long result = solution(4, 2, new long[] { -2, 0, 1, 3 });
+    System.out.println(result);
+  }
+}
