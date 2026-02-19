e748417efd42638d9a714cbded49c01b5c4df812
msa-developer <shoaib9542186823@gmail.com>
2026-02-19 17:56:32 +0530

    added proper patterns to understad

--- Triplets with Smaller Sum/Main.java
+++ Triplets with Smaller Sum/Main.java
@@ -4,8 +4,17 @@
 public class Main {
 
   public static long solution(int n, int sum, long arr[]) {
-
-    return 0;
+    long smallestSum = (arr[0] + arr[1] + arr[2]);
+    int k = arr.length - 1;
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        long store = (arr[i] + arr[j] + arr[k]);
+        if (smallestSum > store) {
+          smallestSum = store;
+        }
+      }
+    }
+    return smallestSum;
   }
 
   public static void main(String[] args) {
